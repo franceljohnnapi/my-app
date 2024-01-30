dee172742fa1b3d647ff4d373fd8f1567cb40fad
francel-john-napi <dxd.lux@gmail.com>
2024-01-30 11:22:10 +0800

    functions completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { useState } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './App.css';
+
 
 function App() {
   const [url, setUrl] = useState()
   const [shortendUrl, setShortenedUrl] = useState('')
+  const shortenUrl = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      )
+      const data = await response.json()
+      setShortenedUrl(data.result.full_short_link);
+    } catch (e) {
+      alert(e);
+    }
+};
 
   return (
     <div className="app">
       <div className='shortener'>
         <h2>URL shortener</h2>
         {/* form to enter URL to be shortened */}
-        <form>
+        <form onSubmit={shortenUrl}>
           <input
             placeholder='Enter URL'
             value={url}
@@ -19,9 +33,12 @@ function App() {
         </form>
         {/* Section to view shortened URLS */}
         {shortendUrl &&
-          <div className='shortener__viewShot'>
-            {shortendUrl}
-          </div>
+          <div className="shortener__viewShot">
+          {shortendUrl}
+          <CopyToClipboard text={shortendUrl}>
+            <button onClick={() => alert("The URL has been copied")}>copy</button>
+          </CopyToClipboard>
+        </div>
         }
       </div>
     </div>
